2df75b5160ca9ae120ec155351c8af703aace7e9
Alice <ciceribeiroo@gmail.com>
2019-09-15 11:24:13 -0300

    Criação da Classe C

---
+++ Projeto/src/projeto/C.java
@@ -0,0 +1,14 @@
+package projeto;
+
+public class C {
+    String C1;
+    int C2;
+    
+    public void MC1(){
+        System.out.println("MC1");
+    }
+    public void MC2(){
+        System.out.println("MC2");
+    }
+    
+}
