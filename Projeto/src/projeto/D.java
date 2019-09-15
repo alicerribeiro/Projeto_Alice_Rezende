f4946746b6874ae83f1a1d731db211e759d6e694
Alice <ciceribeiroo@gmail.com>
2019-09-15 13:59:07 -0300

    Ir pra tag 2

---
+++ Projeto/src/projeto/D.java
@@ -0,0 +1,10 @@
+package projeto;
+public class D {
+    int D1, D2;
+    public void MD1(){
+        System.out.println("MD1");
+    }
+     public void MD2(){
+        System.out.println("MD2");
+    }
+}
